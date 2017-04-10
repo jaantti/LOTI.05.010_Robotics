f578e821b23b20fe5b2a2e729ba9f37b248bd8ba
Antti Jaaniste <jaantti@gmail.com>
2017-04-10 16:12:31 +0300

    Added comments and version for students.

--- 2017/p10/Path planning/main_tudengitele.py
+++ 2017/p10/Path planning/main_tudengitele.py
@@ -41,46 +41,15 @@ for i, row in enumerate(field):
             if item == 3:
                 finish = FieldPoint(j, i)
 
+# Set up starting node
 start.gScore = 0
 open.append(start)
 
 # Find path using A*
 while len(open) > 0 and finish not in closed:
-    #
-    if len(open) == 1:
-        best = open.pop()
-        closed.append(best)
-    else:
-        open.sort()
-        best = open.pop(0)
-        closed.append(best)
-    if best.type != 3:
-        best.type = 4
-    x = best.x
-    y = best.y
-    toCheck = [[x-1, y-1], [x, y-1], [x+1, y-1], [x-1, y], [x+1, y], [x-1, y+1], [x, y+1], [x+1, y+1]]
+    # Write you code here
 
-    for p in toCheck:
-        if p[0] >= 0 and p[0] < fieldX and p[1] >= 0 and p[1] < fieldY:
-            toCheckNode = nodes[p[0], p[1]]
-            if toCheckNode.passable and toCheckNode not in closed:
-                if toCheckNode not in open:
-                    toCheckNode.parent = best
-                    hs = toCheckNode.dist(finish)
-                    gs = toCheckNode.dist(best)
-                    toCheckNode.gScore = best.gScore + gs
-                    toCheckNode.fScore = hs + toCheckNode.gScore
-                    open.append(toCheckNode)
-                else:
-                    gs = best.gScore + toCheckNode.dist(best)
-                    if gs < toCheckNode.gScore:
-                        hs = toCheckNode.dist(finish)
-                        toCheckNode.fScore = hs + gs
-                        toCheckNode.gScore = gs
-                        toCheckNode.parent = best
     f.updateNp(nodes)
-
-currentPath = finish
 
 # Draw the path
 while not currentPath.equals(start):
---
+++ 2017/p10/Tudengid/Field.py
@@ -0,0 +1,82 @@
+from graphics import *
+import time
+
+class Field:
+    def __init__(self, width, height, gridCount):
+        self.sleep = 0
+        self.win = GraphWin()
+        self.gridCount = gridCount
+        self.width = width
+        self.height = height
+
+    def createWindow(self):
+        self.win = GraphWin('Path planning', 500, 500)
+        self.win.setCoords(0, self.gridCount, self.gridCount, 0)
+        self.win.setBackground("white")
+
+    def update(self, stateArray):
+        for i, row in enumerate(stateArray):
+            for j, item in enumerate(row):
+                time.sleep(self.sleep)
+                sq = Rectangle(Point(j, i), Point(j + 1, i + 1))
+                sq.draw(self.win)
+                # Passable terrain
+                if item == 0:
+                    sq.setFill(color_rgb(230, 230, 200))
+                # Obstacle
+                if item == 1:
+                    sq.setFill(color_rgb(0, 0, 0))
+                # Start
+                if item == 2:
+                    sq.setFill("green")
+                # End
+                if item == 3:
+                    sq.setFill("red")
+                # Visited spaces
+                if item == 4:
+                    sq.setFill(color_rgb(220, 170, 220))
+                # Path
+                if item == 5:
+                    sq.setFill(color_rgb(220, 130, 220))
+
+    def updateNp(self, npArray):
+        #self.win.delete('all')
+        for i in npArray:
+            for item in i:
+                time.sleep(self.sleep)
+                sq = Rectangle(Point(item.x, item.y), Point(item.x + 1, item.y + 1))
+                sq.draw(self.win)
+                #tx = Text(Point(item.x+0.5, item.y+0.5), str(item.parent))
+                #tx.draw(self.win)
+                if item.parent is not None:
+                    circ = Circle(Point(item.x+0.5, item.y+0.5), 0.1)
+                    circ.draw(self.win)
+                    linex = (item.x - item.parent.x)*0.5
+                    liney = (item.y - item.parent.y)*0.5
+                    line = Line(Point(item.x+0.5, item.y+0.5), Point(item.x+0.5 - linex, item.y+0.5-liney))
+                    line.draw(self.win)
+                # Passable terrain
+                if item.type == 0:
+                    sq.setFill(color_rgb(230, 230, 200))
+                # Obstacle
+                if item.type == 1:
+                    sq.setFill(color_rgb(0, 0, 0))
+                # Start
+                if item.type == 2:
+                    sq.setFill("green")
+                # End
+                if item.type == 3:
+                    sq.setFill("red")
+                # Visited spaces
+                if item.type == 4:
+                    sq.setFill(color_rgb(220, 170, 220))
+                # Path
+                if item.type == 5:
+                    sq.setFill(color_rgb(220, 130, 220))
+
+    def closeOnMouse(self):
+        self.win.getMouse()
+        self.win.close()
+
+    def setSleepTime(self, time):
+        self.sleep = time
